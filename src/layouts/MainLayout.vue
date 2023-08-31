781330acc20b27ed3c8a5735e764b89ce6c5a8ad
franyori <franyori@gmail.com>
2023-08-31 16:43:04 -0400

    crud Almacen layaout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -459,7 +459,7 @@ const $q = useQuasar();
 const userName = localStorage.getItem('usuario');
 const leftDrawerOpen = ref(true);
 const darkMode = ref(false);
-function toggleLeftDrawer() {
+function toggleLeftDrawer () {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 const darkApply = () => {
